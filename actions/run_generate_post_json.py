ba776ea95f769a26665a9c60178d6a8cf6f09c1c
xrmtech-isk <klimov@x-rm.ru>
2023-02-21 18:55:32 +0300

    send config param

--- actions/run_generate_post_json.py
+++ actions/run_generate_post_json.py
@@ -14,12 +14,19 @@ class RunGenerate(XRMBaseAction):
 
     def run(self, address):
         #self.login()
-        data = {"service": "1", "id": "2", "action": "3"}
+        data = {"site_primary_url": self.config['01_site_primary_url'],\
+                "site_primary_username": self.config['02_site_primary_username'],\
+                "site_primary_password": self.config['03_site_primary_password'],\
+                "site_secondary_url": self.config['04_site_secondary_url'],\
+                "site_secondary_username": self.config['05_site_secondary_username'], \
+                "site_secondary_password": self.config['06_site_secondary_password']}
+                
         req = self.session.post(address, data=data)
         print(req.text)
 
         #print (self.config['01_site_primary_url'])
         #print (self.config['02_site_primary_username'])
         #print (self.config['03_site_primary_password'])
+        
 
         return True
