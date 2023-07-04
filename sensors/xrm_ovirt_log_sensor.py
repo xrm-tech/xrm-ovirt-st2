d5cb35166afc1ec56012f40f51d6fa5a57461a04
xrmtech-isk <119593737+xrmtech-isk@users.noreply.github.com>
2023-07-04 19:02:56 +0300

    Update xrm_ovirt_log_sensor.py

--- sensors/xrm_ovirt_log_sensor.py
+++ sensors/xrm_ovirt_log_sensor.py
@@ -52,7 +52,7 @@ class XRMOvirtLogSensor(PollingSensor):
             
             lines_from_log.append(list(self._string_ref.values())[0]); # add 1 line for debug
            
-            for line in line_from_log: 
+            for line in lines_from_log: 
                 for search_string in self._string_ref.keys():
                     if line.casefold() == search_string.casefold():
                         trigger = self._string_ref[search_string];
